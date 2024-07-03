796bdf0d6933303e93f44fdcce1245ce1e25cf39
忘忧北萱草 <wybxc@stu.pku.edu.cn>
2024-07-03 09:40:23 +0800

    feat: function signature

--- crates/rustc_codegen_c/src/base.rs
+++ crates/rustc_codegen_c/src/base.rs
@@ -1,9 +1,59 @@
-use rustc_codegen_ssa::ModuleCodegen;
+use std::time::Instant;
+
+use rustc_codegen_ssa::mono_item::MonoItemExt;
+use rustc_codegen_ssa::{ModuleCodegen, ModuleKind};
+use rustc_middle::dep_graph;
 use rustc_middle::ty::TyCtxt;
 
+use crate::builder::Builder;
+use crate::context::CodegenCx;
+use crate::module::Module;
+
+// note: parallel
+// it seems this function will be invoked parallelly (if parallel codegen is enabled)
+
 pub fn compile_codegen_unit(
-    _tcx: TyCtxt<'_>,
-    _cgu_name: rustc_span::Symbol,
-) -> (ModuleCodegen<()>, u64) {
-    todo!()
+    tcx: TyCtxt<'_>,
+    cgu_name: rustc_span::Symbol,
+) -> (ModuleCodegen<Module>, u64) {
+    let start_time = Instant::now();
+
+    let dep_node = tcx.codegen_unit(cgu_name).codegen_dep_node(tcx);
+    let (module, _) = tcx.dep_graph.with_task(
+        dep_node,
+        tcx,
+        cgu_name,
+        module_codegen,
+        Some(dep_graph::hash_result),
+    );
+
+    let time_to_codegen = start_time.elapsed();
+    let cost = time_to_codegen
+        .as_secs()
+        .saturating_mul(1_000_000_000)
+        .saturating_add(time_to_codegen.subsec_nanos() as u64);
+
+    (module, cost)
+}
+
+fn module_codegen(tcx: TyCtxt<'_>, cgu_name: rustc_span::Symbol) -> ModuleCodegen<Module> {
+    let cgu = tcx.codegen_unit(cgu_name);
+
+    let cx = CodegenCx::new(tcx);
+
+    let mono_items = cgu.items_in_deterministic_order(tcx);
+    for &(mono_item, data) in &mono_items {
+        mono_item.predefine::<Builder<'_, '_>>(&cx, data.linkage, data.visibility);
+    }
+
+    // ... and now that we have everything pre-defined, fill out those definitions.
+    for &(mono_item, _) in &mono_items {
+        mono_item.define::<Builder<'_, '_>>(&cx);
+    }
+
+    ModuleCodegen {
+        name: cgu_name.to_string(),
+        module_llvm: cx.finish(),
+        kind: ModuleKind::Regular,
+    }
 }
---
+++ example/mini_core.rs
@@ -0,0 +1,39 @@
+#![feature(no_core, lang_items)]
+#![no_core]
+#![allow(internal_features)]
+
+#[lang = "sized"]
+pub trait Sized {}
+
+#[lang = "copy"]
+pub unsafe trait Copy {}
+
+unsafe impl Copy for bool {}
+unsafe impl Copy for u8 {}
+unsafe impl Copy for u16 {}
+unsafe impl Copy for u32 {}
+unsafe impl Copy for u64 {}
+unsafe impl Copy for usize {}
+unsafe impl Copy for i8 {}
+unsafe impl Copy for i16 {}
+unsafe impl Copy for i32 {}
+unsafe impl Copy for isize {}
+unsafe impl Copy for f32 {}
+unsafe impl Copy for f64 {}
+unsafe impl Copy for char {}
+unsafe impl<'a, T: ?Sized> Copy for &'a T {}
+unsafe impl<T: ?Sized> Copy for *const T {}
+unsafe impl<T: ?Sized> Copy for *mut T {}
+
+pub mod libc {
+    #[link(name = "c")]
+    extern "C" {
+        pub fn puts(s: *const u8) -> i32;
+        pub fn printf(format: *const i8, ...) -> i32;
+        pub fn malloc(size: usize) -> *mut u8;
+        pub fn free(ptr: *mut u8);
+        pub fn memcpy(dst: *mut u8, src: *const u8, size: usize);
+        pub fn memmove(dst: *mut u8, src: *const u8, size: usize);
+        pub fn strncpy(dst: *mut u8, src: *const u8, size: usize);
+    }
+}
