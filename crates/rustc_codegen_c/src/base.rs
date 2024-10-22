Wybxc/rustc_codegen_c_ospp

feat: pretty printing

crates/rustc_codegen_c/src/base.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 
+use rustc_codegen_c_ast::{ModuleArena, ModuleCtxt};
 use rustc_codegen_ssa::mono_item::MonoItemExt;
 use rustc_codegen_ssa::{ModuleCodegen, ModuleKind};
 use rustc_middle::dep_graph;
@@ -7,7 +8,9 @@ use rustc_middle::ty::TyCtxt;
 
 use crate::builder::Builder;
 use crate::context::CodegenCx;
-use crate::module::ModuleContext;
+
+/// Needed helper functions
+const HELPER: &str = include_str!("./helper.h");
 
 // note: parallel
 // it seems this function will be invoked parallelly (if parallel codegen is enabled)
@@ -15,7 +18,7 @@ use crate::module::ModuleContext;
 pub fn compile_codegen_unit(
     tcx: TyCtxt<'_>,
     cgu_name: rustc_span::Symbol,
-) -> (ModuleCodegen<ModuleContext>, u64) {
+) -> (ModuleCodegen<String>, u64) {
     let start_time = Instant::now();
 
     let dep_node = tcx.codegen_unit(cgu_name).codegen_dep_node(tcx);
@@ -36,24 +39,23 @@ pub fn compile_codegen_unit(
     (module, cost)
 }
 
-fn module_codegen(tcx: TyCtxt<'_>, cgu_name: rustc_span::Symbol) -> ModuleCodegen<ModuleContext> {
+fn module_codegen(tcx: TyCtxt<'_>, cgu_name: rustc_span::Symbol) -> ModuleCodegen<String> {
     let cgu = tcx.codegen_unit(cgu_name);
 
-    let cx = CodegenCx::new(tcx);
+    let mcx = ModuleArena::new(HELPER);
+    let mcx = ModuleCtxt(&mcx);
+    let cx = CodegenCx::new(tcx, mcx);
 
     let mono_items = cgu.items_in_deterministic_order(tcx);
     for &(mono_item, data) in &mono_items {
-        mono_item.predefine::<Builder<'_, '_>>(&cx, data.linkage, data.visibility);
+        mono_item.predefine::<Builder<'_, '_, '_>>(&cx, data.linkage, data.visibility);
     }
 
     // ... and now that we have everything pre-defined, fill out those definitions.
     for &(mono_item, _) in &mono_items {
-        mono_item.define::<Builder<'_, '_>>(&cx);
+        mono_item.define::<Builder<'_, '_, '_>>(&cx);
     }
 
-    ModuleCodegen {
-        name: cgu_name.to_string(),
-        module_llvm: cx.finish(),
-        kind: ModuleKind::Regular,
-    }
+    let module = mcx.to_string();
+    ModuleCodegen { name: cgu_name.to_string(), module_llvm: module, kind: ModuleKind::Regular }
 }

crates/rustc_codegen_c/src/builder.rs
@@ -3,6 +3,8 @@
 use std::ops::Deref;
 
 use rustc_abi::{HasDataLayout, TargetDataLayout};
+use rustc_codegen_c_ast::func::CFunc;
+use rustc_codegen_c_ast::r#type::CTy;
 use rustc_codegen_ssa::traits::{BackendTypes, BuilderMethods, HasCodegen};
 use rustc_middle::ty::layout::{
     FnAbiError, FnAbiOfHelpers, FnAbiRequest, HasParamEnv, HasTyCtxt, LayoutError, LayoutOfHelpers,
@@ -12,10 +14,7 @@ use rustc_middle::ty::{ParamEnv, Ty, TyCtxt};
 use rustc_target::abi::call::FnAbi;
 use rustc_target::spec::{HasTargetSpec, Target};
 
-use crate::c_expr;
-use crate::context::{CFunctionBuilder, CodegenCx};
-use crate::module::{CDecl, CExpr, CStmt, CType};
-use crate::utils::slab::Id;
+use crate::context::CodegenCx;
 
 mod abi;
 mod asm;
@@ -24,60 +23,60 @@ mod debug_info;
 mod intrinsic_call;
 mod r#static;
 
-pub struct Builder<'a, 'tcx> {
-    pub cx: &'a CodegenCx<'tcx>,
-    bb: Id<CFunctionBuilder>,
+pub struct Builder<'a, 'tcx, 'mx> {
+    pub cx: &'a CodegenCx<'tcx, 'mx>,
+    bb: CFunc<'mx>,
 }
 
-impl<'a, 'tcx> Deref for Builder<'a, 'tcx> {
-    type Target = CodegenCx<'tcx>;
+impl<'a, 'tcx, 'mx> Deref for Builder<'a, 'tcx, 'mx> {
+    type Target = CodegenCx<'tcx, 'mx>;
 
     fn deref<'b>(&'b self) -> &'a Self::Target {
         self.cx
     }
 }
 
-impl<'tcx> HasCodegen<'tcx> for Builder<'_, 'tcx> {
-    type CodegenCx = CodegenCx<'tcx>;
+impl<'tcx, 'mx> HasCodegen<'tcx> for Builder<'_, 'tcx, 'mx> {
+    type CodegenCx = CodegenCx<'tcx, 'mx>;
 }
 
-impl<'tcx> HasDataLayout for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasDataLayout for Builder<'_, 'tcx, 'mx> {
     fn data_layout(&self) -> &TargetDataLayout {
         todo!()
     }
 }
 
-impl<'tcx> HasTyCtxt<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasTyCtxt<'tcx> for Builder<'_, 'tcx, 'mx> {
     fn tcx(&self) -> TyCtxt<'tcx> {
         self.cx.tcx()
     }
 }
 
-impl<'tcx> HasParamEnv<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasParamEnv<'tcx> for Builder<'_, 'tcx, 'mx> {
     fn param_env(&self) -> ParamEnv<'tcx> {
         self.cx.param_env()
     }
 }
 
-impl<'tcx> BackendTypes for Builder<'_, 'tcx> {
-    type Value = <CodegenCx<'tcx> as BackendTypes>::Value;
-    type Function = <CodegenCx<'tcx> as BackendTypes>::Function;
-    type BasicBlock = <CodegenCx<'tcx> as BackendTypes>::BasicBlock;
-    type Type = <CodegenCx<'tcx> as BackendTypes>::Type;
-    type Funclet = <CodegenCx<'tcx> as BackendTypes>::Funclet;
+impl<'tcx, 'mx> BackendTypes for Builder<'_, 'tcx, 'mx> {
+    type Value = <CodegenCx<'tcx, 'mx> as BackendTypes>::Value;
+    type Function = <CodegenCx<'tcx, 'mx> as BackendTypes>::Function;
+    type BasicBlock = <CodegenCx<'tcx, 'mx> as BackendTypes>::BasicBlock;
+    type Type = <CodegenCx<'tcx, 'mx> as BackendTypes>::Type;
+    type Funclet = <CodegenCx<'tcx, 'mx> as BackendTypes>::Funclet;
 
-    type DIScope = <CodegenCx<'tcx> as BackendTypes>::DIScope;
-    type DILocation = <CodegenCx<'tcx> as BackendTypes>::DILocation;
-    type DIVariable = <CodegenCx<'tcx> as BackendTypes>::DIVariable;
+    type DIScope = <CodegenCx<'tcx, 'mx> as BackendTypes>::DIScope;
+    type DILocation = <CodegenCx<'tcx, 'mx> as BackendTypes>::DILocation;
+    type DIVariable = <CodegenCx<'tcx, 'mx> as BackendTypes>::DIVariable;
 }
 
-impl<'tcx> HasTargetSpec for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasTargetSpec for Builder<'_, 'tcx, 'mx> {
     fn target_spec(&self) -> &Target {
         todo!()
     }
 }
 
-impl<'tcx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx, 'mx> {
     type LayoutOfResult = TyAndLayout<'tcx>;
 
     fn handle_layout_err(&self, err: LayoutError<'tcx>, span: rustc_span::Span, ty: Ty<'tcx>) -> ! {
@@ -85,7 +84,7 @@ impl<'tcx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx> {
     }
 }
 
-impl<'tcx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx, 'mx> {
     type FnAbiOfResult = &'tcx FnAbi<'tcx, Ty<'tcx>>;
 
     fn handle_fn_abi_err(
@@ -98,7 +97,7 @@ impl<'tcx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx> {
     }
 }
 
-impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
+impl<'a, 'tcx, 'mx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx, 'mx> {
     fn build(cx: &'a Self::CodegenCx, llbb: Self::BasicBlock) -> Self {
         Self { cx, bb: llbb }
     }
@@ -128,13 +127,11 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
     }
 
     fn ret_void(&mut self) {
-        let mut functions = self.cx.functions.borrow_mut();
-        functions[self.bb].push_stmt(CStmt::Return(None));
+        self.bb.0.push_stmt(self.cx.mcx.ret(None));
     }
 
     fn ret(&mut self, v: Self::Value) {
-        let mut functions = self.cx.functions.borrow_mut();
-        functions[self.bb].push_stmt(CStmt::Return(Some(Box::new(CExpr::Value(v)))));
+        self.bb.0.push_stmt(self.cx.mcx.ret(Some(self.cx.mcx.value(v))))
     }
 
     fn br(&mut self, dest: Self::BasicBlock) {
@@ -510,33 +507,23 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
     /// integer via explicit conversion. Then, use a helper function to cast the
     /// result to a signed integer.
     fn intcast(&mut self, val: Self::Value, dest_ty: Self::Type, is_signed: bool) -> Self::Value {
-        let mut functions = self.cx.functions.borrow_mut();
-        let function = &mut functions[self.bb];
-        let ret = function.next_value();
+        let mcx = self.cx.mcx;
+        let ret = self.bb.0.next_local_var();
 
-        let dest = if let CType::Primitive(ty) = dest_ty { ty } else { unreachable!() };
-        let cast = if !dest.is_signed() {
-            CExpr::Cast { ty: CType::Primitive(dest), expr: Box::new(CExpr::Value(val)) }
-        } else {
-            let cast = CExpr::Cast {
-                ty: CType::Primitive(dest.to_unsigned()),
-                expr: Box::new(CExpr::Value(val)),
-            };
-            CExpr::Call {
-                callee: c_expr!("__rust_utos"),
-                args: vec![
-                    c_expr!(dest.to_unsigned().to_string()),
-                    c_expr!(dest.to_string()),
+        let dest = if let CTy::Primitive(ty) = dest_ty { ty } else { unreachable!() };
+        let mut cast = mcx.cast(CTy::Primitive(dest), mcx.value(val));
+        if dest.is_signed() {
+            cast = mcx.call(
+                mcx.raw("__rust_utos"),
+                vec![
+                    mcx.raw(dest.to_unsigned().to_str()),
+                    mcx.raw(dest.to_str()),
                     cast,
-                    c_expr!(dest.max_value()),
+                    mcx.raw(dest.max_value()),
                 ],
-            }
-        };
-        function.push_stmt(CStmt::Decl(Box::new(CDecl::Var {
-            name: ret,
-            ty: dest_ty,
-            init: Some(cast),
-        })));
+            );
+        }
+        self.bb.0.push_stmt(mcx.decl_stmt(mcx.var(ret, dest_ty, Some(cast))));
         ret
     }
 

crates/rustc_codegen_c/src/context.rs
@@ -3,6 +3,10 @@
 use std::cell::RefCell;
 
 use rustc_abi::{HasDataLayout, TargetDataLayout};
+use rustc_codegen_c_ast::expr::CValue;
+use rustc_codegen_c_ast::func::CFunc;
+use rustc_codegen_c_ast::r#type::CTy;
+use rustc_codegen_c_ast::ModuleCtxt;
 use rustc_codegen_ssa::traits::BackendTypes;
 use rustc_hash::FxHashMap;
 use rustc_middle::ty::layout::{
@@ -12,9 +16,6 @@ use rustc_middle::ty::layout::{
 use rustc_middle::ty::{Instance, ParamEnv, Ty, TyCtxt};
 use rustc_target::abi::call::FnAbi;
 use rustc_target::spec::{HasTargetSpec, Target};
-
-use crate::module::{CDecl, CFunction, CStmt, CType, CValue, ModuleContext};
-use crate::utils::slab::{Id, Slab};
 
 mod asm;
 mod base_type;
@@ -26,72 +27,55 @@ mod pre_define;
 mod r#static;
 mod type_membership;
 
-pub struct CodegenCx<'tcx> {
+pub struct CodegenCx<'tcx, 'mx> {
     pub tcx: TyCtxt<'tcx>,
-    pub mcx: ModuleContext,
-    pub function_instances: RefCell<FxHashMap<Instance<'tcx>, Id<CFunctionBuilder>>>,
-    // TODO: better inner mutablity for slab
-    pub functions: RefCell<Slab<CFunctionBuilder>>,
+    pub mcx: ModuleCtxt<'mx>,
+    pub function_instances: RefCell<FxHashMap<Instance<'tcx>, CFunc<'mx>>>,
 }
 
-impl<'tcx> CodegenCx<'tcx> {
-    pub fn new(tcx: TyCtxt<'tcx>) -> Self {
-        let mcx = ModuleContext::new();
-        Self {
-            tcx,
-            mcx,
-            function_instances: RefCell::new(FxHashMap::default()),
-            functions: RefCell::new(Slab::default()),
-        }
-    }
-
-    pub fn finish(mut self) -> ModuleContext {
-        for function in self.functions.borrow().iter() {
-            self.mcx.module.decls.push(function.decl());
-        }
-        for function in self.functions.into_inner() {
-            self.mcx.module.decls.push(CDecl::Function(function.build()));
-        }
-        self.mcx
+impl<'tcx, 'mx> CodegenCx<'tcx, 'mx> {
+    pub fn new(tcx: TyCtxt<'tcx>, mcx: ModuleCtxt<'mx>) -> Self {
+        mcx.module().push_include("stdint.h");
+        Self { tcx, mcx, function_instances: RefCell::new(FxHashMap::default()) }
     }
 }
 
-impl<'tcx> BackendTypes for CodegenCx<'tcx> {
+impl<'tcx, 'mx> BackendTypes for CodegenCx<'tcx, 'mx> {
     type Value = CValue;
-    type Function = Id<CFunctionBuilder>;
-    type BasicBlock = Id<CFunctionBuilder>;
-    type Type = CType;
+    type Function = CFunc<'mx>;
+    type BasicBlock = CFunc<'mx>;
+    type Type = CTy<'mx>;
     type Funclet = ();
     type DIScope = ();
     type DILocation = ();
     type DIVariable = ();
 }
 
-impl<'tcx> HasTargetSpec for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasTargetSpec for CodegenCx<'tcx, 'mx> {
     fn target_spec(&self) -> &Target {
         todo!()
     }
 }
 
-impl<'tcx> HasParamEnv<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasParamEnv<'tcx> for CodegenCx<'tcx, 'mx> {
     fn param_env(&self) -> ParamEnv<'tcx> {
         ParamEnv::reveal_all()
     }
 }
 
-impl<'tcx> HasTyCtxt<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasTyCtxt<'tcx> for CodegenCx<'tcx, 'mx> {
     fn tcx(&self) -> TyCtxt<'tcx> {
         self.tcx
     }
 }
 
-impl HasDataLayout for CodegenCx<'_> {
+impl<'tcx, 'mx> HasDataLayout for CodegenCx<'tcx, 'mx> {
     fn data_layout(&self) -> &TargetDataLayout {
         todo!()
     }
 }
 
-impl<'tcx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx, 'mx> {
     type LayoutOfResult = TyAndLayout<'tcx>;
 
     fn handle_layout_err(&self, err: LayoutError<'tcx>, span: rustc_span::Span, ty: Ty<'tcx>) -> ! {
@@ -99,7 +83,7 @@ impl<'tcx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx> {
     }
 }
 
-impl<'tcx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx, 'mx> {
     type FnAbiOfResult = &'tcx FnAbi<'tcx, Ty<'tcx>>;
 
     fn handle_fn_abi_err(
@@ -109,46 +93,5 @@ impl<'tcx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx> {
         fn_abi_request: FnAbiRequest<'tcx>,
     ) -> ! {
         todo!()
-    }
-}
-
-pub struct CFunctionBuilder {
-    pub name: String,
-    pub ty: CType,
-    pub params: Vec<(CType, CValue)>,
-    pub body: Vec<CStmt>,
-
-    var_counter: usize,
-}
-
-impl CFunctionBuilder {
-    pub fn new(name: String, ty: CType, params: Vec<CType>) -> Self {
-        let params: Vec<_> =
-            params.into_iter().enumerate().map(|(i, ty)| (ty, CValue::Local(i))).collect();
-        let var_counter = params.len();
-
-        Self { name, ty, params, body: Vec::new(), var_counter }
-    }
-
-    pub fn build(self) -> CFunction {
-        CFunction { name: self.name, ty: self.ty, params: self.params, body: self.body }
-    }
-
-    pub fn decl(&self) -> CDecl {
-        CDecl::FunctionDecl {
-            name: self.name.clone(),
-            ty: self.ty,
-            params: self.params.iter().map(|(ty, _)| *ty).collect(),
-        }
-    }
-
-    pub fn next_value(&mut self) -> CValue {
-        let val = CValue::Local(self.var_counter);
-        self.var_counter += 1;
-        val
-    }
-
-    pub fn push_stmt(&mut self, stmt: CStmt) {
-        self.body.push(stmt);
     }
 }

rust-lang/rustc_codegen_c

crates/rustc_codegen_c/src/base.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 
+use rustc_codegen_c_ast::{ModuleArena, ModuleCtx};
 use rustc_codegen_ssa::mono_item::MonoItemExt;
 use rustc_codegen_ssa::{ModuleCodegen, ModuleKind};
 use rustc_middle::dep_graph;
@@ -7,7 +8,9 @@ use rustc_middle::ty::TyCtxt;
 
 use crate::builder::Builder;
 use crate::context::CodegenCx;
-use crate::module::ModuleContext;
+
+/// Needed helper functions
+const HELPER: &str = include_str!("./helper.h");
 
 // note: parallel
 // it seems this function will be invoked parallelly (if parallel codegen is enabled)
@@ -15,7 +18,7 @@ use crate::module::ModuleContext;
 pub fn compile_codegen_unit(
     tcx: TyCtxt<'_>,
     cgu_name: rustc_span::Symbol,
-) -> (ModuleCodegen<ModuleContext>, u64) {
+) -> (ModuleCodegen<String>, u64) {
     let start_time = Instant::now();
 
     let dep_node = tcx.codegen_unit(cgu_name).codegen_dep_node(tcx);
@@ -36,24 +39,23 @@ pub fn compile_codegen_unit(
     (module, cost)
 }
 
-fn module_codegen(tcx: TyCtxt<'_>, cgu_name: rustc_span::Symbol) -> ModuleCodegen<ModuleContext> {
+fn module_codegen(tcx: TyCtxt<'_>, cgu_name: rustc_span::Symbol) -> ModuleCodegen<String> {
     let cgu = tcx.codegen_unit(cgu_name);
 
-    let cx = CodegenCx::new(tcx);
+    let mcx = ModuleArena::new(HELPER);
+    let mcx = ModuleCtx(&mcx);
+    let cx = CodegenCx::new(tcx, mcx);
 
     let mono_items = cgu.items_in_deterministic_order(tcx);
     for &(mono_item, data) in &mono_items {
-        mono_item.predefine::<Builder<'_, '_>>(&cx, data.linkage, data.visibility);
+        mono_item.predefine::<Builder<'_, '_, '_>>(&cx, data.linkage, data.visibility);
     }
 
     // ... and now that we have everything pre-defined, fill out those definitions.
     for &(mono_item, _) in &mono_items {
-        mono_item.define::<Builder<'_, '_>>(&cx);
+        mono_item.define::<Builder<'_, '_, '_>>(&cx);
     }
 
-    ModuleCodegen {
-        name: cgu_name.to_string(),
-        module_llvm: cx.finish(),
-        kind: ModuleKind::Regular,
-    }
+    let module = mcx.to_string();
+    ModuleCodegen { name: cgu_name.to_string(), module_llvm: module, kind: ModuleKind::Regular }
 }

crates/rustc_codegen_c/src/builder.rs
@@ -3,6 +3,7 @@
 use std::ops::Deref;
 
 use rustc_abi::{HasDataLayout, TargetDataLayout};
+use rustc_codegen_c_ast::func::CFunc;
 use rustc_codegen_ssa::traits::{BackendTypes, BuilderMethods, HasCodegen};
 use rustc_middle::ty::layout::{
     FnAbiError, FnAbiOfHelpers, FnAbiRequest, HasParamEnv, HasTyCtxt, LayoutError, LayoutOfHelpers,
@@ -12,10 +13,7 @@ use rustc_middle::ty::{ParamEnv, Ty, TyCtxt};
 use rustc_target::abi::call::FnAbi;
 use rustc_target::spec::{HasTargetSpec, Target};
 
-use crate::c_expr;
-use crate::context::{CFunctionBuilder, CodegenCx};
-use crate::module::{CDecl, CExpr, CStmt, CType};
-use crate::utils::slab::Id;
+use crate::context::CodegenCx;
 
 mod abi;
 mod asm;
@@ -24,60 +22,60 @@ mod debug_info;
 mod intrinsic_call;
 mod r#static;
 
-pub struct Builder<'a, 'tcx> {
-    pub cx: &'a CodegenCx<'tcx>,
-    bb: Id<CFunctionBuilder>,
+pub struct Builder<'a, 'tcx, 'mx> {
+    pub cx: &'a CodegenCx<'tcx, 'mx>,
+    bb: CFunc<'mx>,
 }
 
-impl<'a, 'tcx> Deref for Builder<'a, 'tcx> {
-    type Target = CodegenCx<'tcx>;
+impl<'a, 'tcx, 'mx> Deref for Builder<'a, 'tcx, 'mx> {
+    type Target = CodegenCx<'tcx, 'mx>;
 
     fn deref<'b>(&'b self) -> &'a Self::Target {
         self.cx
     }
 }
 
-impl<'tcx> HasCodegen<'tcx> for Builder<'_, 'tcx> {
-    type CodegenCx = CodegenCx<'tcx>;
+impl<'tcx, 'mx> HasCodegen<'tcx> for Builder<'_, 'tcx, 'mx> {
+    type CodegenCx = CodegenCx<'tcx, 'mx>;
 }
 
-impl<'tcx> HasDataLayout for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasDataLayout for Builder<'_, 'tcx, 'mx> {
     fn data_layout(&self) -> &TargetDataLayout {
         todo!()
     }
 }
 
-impl<'tcx> HasTyCtxt<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasTyCtxt<'tcx> for Builder<'_, 'tcx, 'mx> {
     fn tcx(&self) -> TyCtxt<'tcx> {
         self.cx.tcx()
     }
 }
 
-impl<'tcx> HasParamEnv<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasParamEnv<'tcx> for Builder<'_, 'tcx, 'mx> {
     fn param_env(&self) -> ParamEnv<'tcx> {
         self.cx.param_env()
     }
 }
 
-impl<'tcx> BackendTypes for Builder<'_, 'tcx> {
-    type Value = <CodegenCx<'tcx> as BackendTypes>::Value;
-    type Function = <CodegenCx<'tcx> as BackendTypes>::Function;
-    type BasicBlock = <CodegenCx<'tcx> as BackendTypes>::BasicBlock;
-    type Type = <CodegenCx<'tcx> as BackendTypes>::Type;
-    type Funclet = <CodegenCx<'tcx> as BackendTypes>::Funclet;
+impl<'tcx, 'mx> BackendTypes for Builder<'_, 'tcx, 'mx> {
+    type Value = <CodegenCx<'tcx, 'mx> as BackendTypes>::Value;
+    type Function = <CodegenCx<'tcx, 'mx> as BackendTypes>::Function;
+    type BasicBlock = <CodegenCx<'tcx, 'mx> as BackendTypes>::BasicBlock;
+    type Type = <CodegenCx<'tcx, 'mx> as BackendTypes>::Type;
+    type Funclet = <CodegenCx<'tcx, 'mx> as BackendTypes>::Funclet;
 
-    type DIScope = <CodegenCx<'tcx> as BackendTypes>::DIScope;
-    type DILocation = <CodegenCx<'tcx> as BackendTypes>::DILocation;
-    type DIVariable = <CodegenCx<'tcx> as BackendTypes>::DIVariable;
+    type DIScope = <CodegenCx<'tcx, 'mx> as BackendTypes>::DIScope;
+    type DILocation = <CodegenCx<'tcx, 'mx> as BackendTypes>::DILocation;
+    type DIVariable = <CodegenCx<'tcx, 'mx> as BackendTypes>::DIVariable;
 }
 
-impl<'tcx> HasTargetSpec for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> HasTargetSpec for Builder<'_, 'tcx, 'mx> {
     fn target_spec(&self) -> &Target {
         todo!()
     }
 }
 
-impl<'tcx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx, 'mx> {
     type LayoutOfResult = TyAndLayout<'tcx>;
 
     fn handle_layout_err(&self, err: LayoutError<'tcx>, span: rustc_span::Span, ty: Ty<'tcx>) -> ! {
@@ -85,7 +83,7 @@ impl<'tcx> LayoutOfHelpers<'tcx> for Builder<'_, 'tcx> {
     }
 }
 
-impl<'tcx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx> {
+impl<'tcx, 'mx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx, 'mx> {
     type FnAbiOfResult = &'tcx FnAbi<'tcx, Ty<'tcx>>;
 
     fn handle_fn_abi_err(
@@ -98,7 +96,7 @@ impl<'tcx> FnAbiOfHelpers<'tcx> for Builder<'_, 'tcx> {
     }
 }
 
-impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
+impl<'a, 'tcx, 'mx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx, 'mx> {
     fn build(cx: &'a Self::CodegenCx, llbb: Self::BasicBlock) -> Self {
         Self { cx, bb: llbb }
     }
@@ -128,13 +126,11 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
     }
 
     fn ret_void(&mut self) {
-        let mut functions = self.cx.functions.borrow_mut();
-        functions[self.bb].push_stmt(CStmt::Return(None));
+        self.bb.0.push_stmt(self.cx.mcx.ret(None));
     }
 
     fn ret(&mut self, v: Self::Value) {
-        let mut functions = self.cx.functions.borrow_mut();
-        functions[self.bb].push_stmt(CStmt::Return(Some(Box::new(CExpr::Value(v)))));
+        self.bb.0.push_stmt(self.cx.mcx.ret(Some(self.cx.mcx.value(v))))
     }
 
     fn br(&mut self, dest: Self::BasicBlock) {
@@ -510,33 +506,22 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
     /// integer via explicit conversion. Then, use a helper function to cast the
     /// result to a signed integer.
     fn intcast(&mut self, val: Self::Value, dest_ty: Self::Type, is_signed: bool) -> Self::Value {
-        let mut functions = self.cx.functions.borrow_mut();
-        let function = &mut functions[self.bb];
-        let ret = function.next_value();
+        let mcx = self.cx.mcx;
+        let ret = self.bb.0.next_local_var();
 
-        let dest = if let CType::Primitive(ty) = dest_ty { ty } else { unreachable!() };
-        let cast = if !dest.is_signed() {
-            CExpr::Cast { ty: CType::Primitive(dest), expr: Box::new(CExpr::Value(val)) }
-        } else {
-            let cast = CExpr::Cast {
-                ty: CType::Primitive(dest.to_unsigned()),
-                expr: Box::new(CExpr::Value(val)),
-            };
-            CExpr::Call {
-                callee: c_expr!("__rust_utos"),
-                args: vec![
-                    c_expr!(dest.to_unsigned().to_string()),
-                    c_expr!(dest.to_string()),
+        let mut cast = mcx.cast(dest_ty, mcx.value(val));
+        if dest_ty.is_signed() {
+            cast = mcx.call(
+                mcx.raw("__rust_utos"),
+                vec![
+                    mcx.raw(dest_ty.to_unsigned().to_str()),
+                    mcx.raw(dest_ty.to_str()),
                     cast,
-                    c_expr!(dest.max_value()),
+                    mcx.raw(dest_ty.max_value()),
                 ],
-            }
-        };
-        function.push_stmt(CStmt::Decl(Box::new(CDecl::Var {
-            name: ret,
-            ty: dest_ty,
-            init: Some(cast),
-        })));
+            );
+        }
+        self.bb.0.push_stmt(mcx.decl_stmt(mcx.var(ret, dest_ty, Some(cast))));
         ret
     }
 

crates/rustc_codegen_c/src/context.rs
@@ -3,6 +3,10 @@
 use std::cell::RefCell;
 
 use rustc_abi::{HasDataLayout, TargetDataLayout};
+use rustc_codegen_c_ast::expr::CValue;
+use rustc_codegen_c_ast::func::CFunc;
+use rustc_codegen_c_ast::ty::CTy;
+use rustc_codegen_c_ast::ModuleCtx;
 use rustc_codegen_ssa::traits::BackendTypes;
 use rustc_hash::FxHashMap;
 use rustc_middle::ty::layout::{
@@ -12,9 +16,6 @@ use rustc_middle::ty::layout::{
 use rustc_middle::ty::{Instance, ParamEnv, Ty, TyCtxt};
 use rustc_target::abi::call::FnAbi;
 use rustc_target::spec::{HasTargetSpec, Target};
-
-use crate::module::{CDecl, CFunction, CStmt, CType, CValue, ModuleContext};
-use crate::utils::slab::{Id, Slab};
 
 mod asm;
 mod base_type;
@@ -26,72 +27,55 @@ mod pre_define;
 mod r#static;
 mod type_membership;
 
-pub struct CodegenCx<'tcx> {
+pub struct CodegenCx<'tcx, 'mx> {
     pub tcx: TyCtxt<'tcx>,
-    pub mcx: ModuleContext,
-    pub function_instances: RefCell<FxHashMap<Instance<'tcx>, Id<CFunctionBuilder>>>,
-    // TODO: better inner mutablity for slab
-    pub functions: RefCell<Slab<CFunctionBuilder>>,
+    pub mcx: ModuleCtx<'mx>,
+    pub function_instances: RefCell<FxHashMap<Instance<'tcx>, CFunc<'mx>>>,
 }
 
-impl<'tcx> CodegenCx<'tcx> {
-    pub fn new(tcx: TyCtxt<'tcx>) -> Self {
-        let mcx = ModuleContext::new();
-        Self {
-            tcx,
-            mcx,
-            function_instances: RefCell::new(FxHashMap::default()),
-            functions: RefCell::new(Slab::default()),
-        }
-    }
-
-    pub fn finish(mut self) -> ModuleContext {
-        for function in self.functions.borrow().iter() {
-            self.mcx.module.decls.push(function.decl());
-        }
-        for function in self.functions.into_inner() {
-            self.mcx.module.decls.push(CDecl::Function(function.build()));
-        }
-        self.mcx
+impl<'tcx, 'mx> CodegenCx<'tcx, 'mx> {
+    pub fn new(tcx: TyCtxt<'tcx>, mcx: ModuleCtx<'mx>) -> Self {
+        mcx.module().push_include("stdint.h");
+        Self { tcx, mcx, function_instances: RefCell::new(FxHashMap::default()) }
     }
 }
 
-impl<'tcx> BackendTypes for CodegenCx<'tcx> {
-    type Value = CValue;
-    type Function = Id<CFunctionBuilder>;
-    type BasicBlock = Id<CFunctionBuilder>;
-    type Type = CType;
+impl<'tcx, 'mx> BackendTypes for CodegenCx<'tcx, 'mx> {
+    type Value = CValue<'mx>;
+    type Function = CFunc<'mx>;
+    type BasicBlock = CFunc<'mx>;
+    type Type = CTy<'mx>;
     type Funclet = ();
     type DIScope = ();
     type DILocation = ();
     type DIVariable = ();
 }
 
-impl<'tcx> HasTargetSpec for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasTargetSpec for CodegenCx<'tcx, 'mx> {
     fn target_spec(&self) -> &Target {
         todo!()
     }
 }
 
-impl<'tcx> HasParamEnv<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasParamEnv<'tcx> for CodegenCx<'tcx, 'mx> {
     fn param_env(&self) -> ParamEnv<'tcx> {
         ParamEnv::reveal_all()
     }
 }
 
-impl<'tcx> HasTyCtxt<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> HasTyCtxt<'tcx> for CodegenCx<'tcx, 'mx> {
     fn tcx(&self) -> TyCtxt<'tcx> {
         self.tcx
     }
 }
 
-impl HasDataLayout for CodegenCx<'_> {
+impl<'tcx, 'mx> HasDataLayout for CodegenCx<'tcx, 'mx> {
     fn data_layout(&self) -> &TargetDataLayout {
         todo!()
     }
 }
 
-impl<'tcx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx, 'mx> {
     type LayoutOfResult = TyAndLayout<'tcx>;
 
     fn handle_layout_err(&self, err: LayoutError<'tcx>, span: rustc_span::Span, ty: Ty<'tcx>) -> ! {
@@ -99,7 +83,7 @@ impl<'tcx> LayoutOfHelpers<'tcx> for CodegenCx<'tcx> {
     }
 }
 
-impl<'tcx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx> {
+impl<'tcx, 'mx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx, 'mx> {
     type FnAbiOfResult = &'tcx FnAbi<'tcx, Ty<'tcx>>;
 
     fn handle_fn_abi_err(
@@ -109,46 +93,5 @@ impl<'tcx> FnAbiOfHelpers<'tcx> for CodegenCx<'tcx> {
         fn_abi_request: FnAbiRequest<'tcx>,
     ) -> ! {
         todo!()
-    }
-}
-
-pub struct CFunctionBuilder {
-    pub name: String,
-    pub ty: CType,
-    pub params: Vec<(CType, CValue)>,
-    pub body: Vec<CStmt>,
-
-    var_counter: usize,
-}
-
-impl CFunctionBuilder {
-    pub fn new(name: String, ty: CType, params: Vec<CType>) -> Self {
-        let params: Vec<_> =
-            params.into_iter().enumerate().map(|(i, ty)| (ty, CValue::Local(i))).collect();
-        let var_counter = params.len();
-
-        Self { name, ty, params, body: Vec::new(), var_counter }
-    }
-
-    pub fn build(self) -> CFunction {
-        CFunction { name: self.name, ty: self.ty, params: self.params, body: self.body }
-    }
-
-    pub fn decl(&self) -> CDecl {
-        CDecl::FunctionDecl {
-            name: self.name.clone(),
-            ty: self.ty,
-            params: self.params.iter().map(|(ty, _)| *ty).collect(),
-        }
-    }
-
-    pub fn next_value(&mut self) -> CValue {
-        let val = CValue::Local(self.var_counter);
-        self.var_counter += 1;
-        val
-    }
-
-    pub fn push_stmt(&mut self, stmt: CStmt) {
-        self.body.push(stmt);
     }
 }