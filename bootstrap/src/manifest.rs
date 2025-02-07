68dccf9f82183ccfb67fb8cba534fd1c4b276f32
xizheyin <xizheyin@smail.nju.edu.cn>
2025-02-07 23:31:16 +0800

    Build: improve verbose output in manifest
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- bootstrap/src/manifest.rs
+++ bootstrap/src/manifest.rs
@@ -13,36 +13,70 @@ pub struct Manifest {
 impl Manifest {
     /// Builds the rustc codegen c library
     pub fn prepare(&self) {
-        cprintln!("<b>[BUILD]</b> codegen backend");
+        // Build codegen backend
+        if self.verbose {
+            cprintln!("<b>[BUILD]</b> preparing codegen backend");
+            cprintln!("       target: {}", self.codegen_backend().display());
+        } else {
+            cprintln!("<b>[BUILD]</b> codegen backend");
+        }
+
         let mut command = Command::new("cargo");
         command.arg("build").args(["--manifest-path", "crates/Cargo.toml"]);
         if self.verbose {
-            command.args(["-F", "debug"]);
+            command.args(["-v"]);
+            cprintln!("       command: {}", format!("{:?}", command).replace('"', ""));
         }
         if self.release {
             command.arg("--release");
         }
         log::debug!("running {:?}", command);
-        command.status().unwrap();
+        let status = command.status().unwrap();
+        if self.verbose && status.success() {
+            cprintln!("       <g>success</g>");
+        }
 
-        cprintln!("<b>[BUILD]</b> librust_runtime");
+        // Build runtime library
+        if self.verbose {
+            cprintln!("<b>[BUILD]</b> preparing librust_runtime");
+            cprintln!("       output: {}", self.out_dir.display());
+        } else {
+            cprintln!("<b>[BUILD]</b> librust_runtime");
+        }
+
         std::fs::create_dir_all(&self.out_dir).unwrap();
         let cc = std::env::var("CC").unwrap_or("clang".to_string());
+
+        // Compile runtime.c
         let mut command = Command::new(&cc);
         command
             .arg("rust_runtime/rust_runtime.c")
             .arg("-o")
             .arg(self.out_dir.join("rust_runtime.o"))
             .arg("-c");
+        if self.verbose {
+            cprintln!("       compile: {}", format!("{:?}", command).replace('"', ""));
+        }
         log::debug!("running {:?}", command);
-        command.status().unwrap();
+        let status = command.status().unwrap();
+        if self.verbose && status.success() {
+            cprintln!("       <g>success</g>");
+        }
+
+        // Create static library
         let mut command = Command::new("ar");
         command
             .arg("rcs")
             .arg(self.out_dir.join("librust_runtime.a"))
             .arg(self.out_dir.join("rust_runtime.o"));
+        if self.verbose {
+            cprintln!("       archive: {}", format!("{:?}", command).replace('"', ""));
+        }
         log::debug!("running {:?}", command);
-        command.status().unwrap();
+        let status = command.status().unwrap();
+        if self.verbose && status.success() {
+            cprintln!("       <g>success</g>");
+        }
     }
 
     /// The path to the rustc codegen c library
